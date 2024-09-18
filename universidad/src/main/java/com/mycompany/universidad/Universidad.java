897b6ffc427ea11eaa32999af6909384a8bf3643
lusmax <lusmax0128@gmail.com>
2024-09-18 13:53:13 +0200

    test getCarrera method on main

--- universidad/src/main/java/com/mycompany/universidad/Universidad.java
+++ universidad/src/main/java/com/mycompany/universidad/Universidad.java
@@ -1,7 +1,11 @@
 package com.mycompany.universidad;
+
+import dbentidades.DbCarreras;
 import dbentidades.DbConnection;
 import entidades.*;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -10,18 +14,30 @@ import java.sql.Connection;
 public class Universidad {
 
     public static void main(String[] args) {
-       Carreras miCarrera = new Carreras("Historia");
-       
-       String url = "jdbc:mysql://localhost";
-       String DataBase = "universidad";
-       String user = "root";
-       String pass = "123456";
-       
-        
+        Carreras miCarrera = new Carreras("Historia");
+
+        String url = "jdbc:mysql://localhost";
+        String DataBase = "universidad";
+        String user = "root";
+        String pass = "root";
+
+        //TESTING CONNECTION TO DATABASE
         Connection miConexion = DbConnection.getConnection(url, DataBase, user, pass);
-        
+
+        //TESTING SELECT 
+        ResultSet selectCarrera = DbCarreras.getCarrera(miConexion, 0);
+
+        try {
+            while (selectCarrera.next()) {
+                System.out.println(selectCarrera.getString("carrera"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        //TESTING TO CLOSE CONNECTION
         String closeConnection = DbConnection.closeConnection(miConexion);
         System.out.println(closeConnection);
-       
+
     }
 }
