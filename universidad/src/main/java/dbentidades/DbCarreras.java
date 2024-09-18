46d4a6ea1835da72a50d107a556f1bebecb5a339
lusmax <lusmax0128@gmail.com>
2024-09-18 11:55:53 +0200

    create insertCarrera method in DbCarreras

--- universidad/src/main/java/dbentidades/DbCarreras.java
+++ universidad/src/main/java/dbentidades/DbCarreras.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,6 +27,21 @@ public abstract class DbCarreras {
             ex.printStackTrace();
         }
 
+        return result;
+    }
+
+    public static String insertCarrera(Connection connection, String carrera) {
+        String result;
+        String sql = "INSERT INTO carreras (carrera)"
+                + " VALUES (" + carrera + ")";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(sql);
+            result = "New register inserted";
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            result = "Can't insert register";
+        }
         return result;
     }
 
